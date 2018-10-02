14a4e7aa661abe5b0c68751901eb1562e37e2e52
SusinMat <57694766+SusinMat@users.noreply.github.com>
2018-10-02 12:50:19 -0300

    Add several debugs and debugging prints to Emily's code

--- compress_conv/bisubspace_lowrank_example.m
+++ compress_conv/bisubspace_lowrank_example.m
@@ -1,4 +1,4 @@
-W = randn(256, 5, 5, 96); --create random weights with 96 input channels, 256 output channels and 5x5 filters
+W = randn(256, 5, 5, 96); % create random weights with 96 input channels, 256 output channels and 5x5 filters
 
 % Replace first convolutional layer weights with approximated weights
 args.iclust = 48; % number of input clusters
@@ -11,7 +11,7 @@ args.cluster_type = 'kmeans';
 
 fprintf('||W|| = %f \n', norm(W(:)));
 
-[Wapprox, F, C, XY, perm_in, perm_out, num_weights] = bisubspace_lowrank_approx_nosep(double(W), args);
+[Wapprox, F, C, XY, perm_in, perm_out, num_weights] = bisubspace_lowrank_approx(double(W), args);
 L2_err = norm(W(:) - Wapprox(:)) / norm(W(:));
 fprintf('||W - Wapprox|| / ||W|| = %f (*note* error should be high since weights are random and thus have no structure...\n', L2_err);
 
