9cdbfdbd2d0865bc89ccdd4f898c82eab4b2a88f
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2018-11-13 17:52:21 -0200

    Save flatbuffer in flatbuffer_rebuilder.py

--- dump_parser/flatbuffer_rebuilder.py
+++ dump_parser/flatbuffer_rebuilder.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 # Copyright 2018 SUSIN, Matheus
-# 
+#
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-# 
+#
 # 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-# 
+#
 # 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHA    LL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTER    RUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import argparse
@@ -308,14 +308,20 @@ if __name__ == "__main__":
             print(tensor)
         print("----------------")
 
-    sess = tf.Session()
-    tf.global_variables_initializer().run(session=sess)
-    # tf.tables_initializer().run(session=sess)
-    out_tensor = sess.run(graph[-1], {input_placeholder : image})
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run(session=sess)
+        # tf.tables_initializer().run(session=sess)
+        out_tensor = sess.run(graph[-1], {input_placeholder : image})
 
-    sorted_out_tensor = np.flipud(np.sort(out_tensor[0]))
-    indexes = np.argsort(-out_tensor[0])
-    print("Top 5:")
-    for i in range(5):
-        print("%03d : %05.2f%% (%s)" % (indexes[i], sorted_out_tensor[i] * 100, labels[indexes[i]]))
-    # print(sess.run(evaluated_tensors[2], {input_placeholder : image}))
+        # tensorflow 1.11
+        # save flatbuffer
+        converter = tf.contrib.lite.TocoConverter.from_session(sess, [input_placeholder], [graph[-1]])
+        tflite_model = converter.convert()
+        open("reconstructed_model.tflite", "wb").write(tflite_model)
+
+        sorted_out_tensor = np.flipud(np.sort(out_tensor[0]))
+        indexes = np.argsort(-out_tensor[0])
+        print("Top 5:")
+        for i in range(5):
+            print("%03d : %05.2f%% (%s)" % (indexes[i], sorted_out_tensor[i] * 100, labels[indexes[i]]))
+        # print(sess.run(evaluated_tensors[2], {input_placeholder : image}))
