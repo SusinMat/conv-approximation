29b7b9ce2db78397440635e4ec15b9a5121dfec2
SusinMat <57694766+SusinMat@users.noreply.github.com>
2019-03-31 18:47:06 -0300

    Fix get_rand_int using a value of m too large for MatLab

--- permute/fisher-yates.m
+++ permute/fisher-yates.m
@@ -1,12 +1,15 @@
 #!/usr/bin/env octave
 
+global seed;
+
 function [r] = get_rand_int(n)
-	seed = 0;
-	m = 2147483648;
+	global seed;
+	m = 100003;
 	a = 1103515245;
 	c = 12345;
 	seed = mod(a * seed + c, m);
-	r = mod(seed, n) + 1;
+	printf('seed after == %d\n', seed);
+	r = mod(seed, n);
 end
 
 function [array] = random_permutation(n)
@@ -16,13 +19,18 @@ function [array] = random_permutation(n)
 	end
 	for i = n : -1 : 2
 		% j = randi([1, i], 1);
-		j = get_rand_int(i);
-		printf('%d\n', j);
+		j = get_rand_int(i) + 1;
+		printf('j == %d\n', j);
 		aux = array(i);
 		array(i) = array(j);
 		array(j) = aux;
 	end
 end
 
-rand('seed', 0);
-printf('%s\n', mat2str(random_permutation(64)));
+% rand('seed', 0);
+seed = 2500;
+printf('%s\n', mat2str(random_permutation(640)));
+exit()
+for i = 1 : 500
+	get_rand_int(2);
+end
--- permute/fisher-yates.py
+++ permute/fisher-yates.py
@@ -2,13 +2,16 @@
 
 import numpy as np
 
+seed = 2500
+
 def get_rand_int(n):
-    seed = 0
-    m = 2147483648
+    global seed
+    m = 100003
     a = 1103515245
     c = 12345
     seed = (a * seed + c) % m
-    r = seed % n + 1
+    print("seed after == " + str(seed))
+    r = seed % n
     return r
 
 def random_permutation(n):
@@ -17,12 +20,16 @@ def random_permutation(n):
         return array
     for i in range(n - 1, 0, -1):
         # j = np.random.randint(0, i)
-        j = get_rand_int(i + 1) - 1
-        print(j)
+        j = get_rand_int(i + 1)
+        print("j == " + str(j))
         aux = array[i]
         array[i] = array[j]
         array[j] = aux
     return array
 
-np.random.seed(0)
-print(random_permutation(64))
+# np.random.seed(0)
+print(random_permutation(640) + 1)
+exit()
+
+for i in range(500):
+    get_rand_int(2)
