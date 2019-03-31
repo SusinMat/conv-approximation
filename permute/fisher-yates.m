142db5345becb2eec5c28fca5ecce2664265494e
SusinMat <57694766+SusinMat@users.noreply.github.com>
2019-03-31 18:16:28 -0300

    Use the same PRNG for the Numpy and Matlab versions of Fisher-Yates

--- permute/fisher-yates.m
+++ permute/fisher-yates.m
@@ -1,17 +1,28 @@
 #!/usr/bin/env octave
 
+function [r] = get_rand_int(n)
+	seed = 0;
+	m = 2147483648;
+	a = 1103515245;
+	c = 12345;
+	seed = mod(a * seed + c, m);
+	r = mod(seed, n) + 1;
+end
+
 function [array] = random_permutation(n)
 	array = [1 : n];
 	if n == 1
 		return
 	end
 	for i = n : -1 : 2
-		j = randi([1, i], 1);
+		% j = randi([1, i], 1);
+		j = get_rand_int(i);
+		printf('%d\n', j);
 		aux = array(i);
 		array(i) = array(j);
 		array(j) = aux;
 	end
 end
 
-rand('seed', 87958177);
+rand('seed', 0);
 printf('%s\n', mat2str(random_permutation(64)));
--- permute/fisher-yates.py
+++ permute/fisher-yates.py
@@ -2,12 +2,23 @@
 
 import numpy as np
 
+def get_rand_int(n):
+    seed = 0
+    m = 2147483648
+    a = 1103515245
+    c = 12345
+    seed = (a * seed + c) % m
+    r = seed % n + 1
+    return r
+
 def random_permutation(n):
     array = np.arange(n, dtype=np.int)
     if n == 1:
         return array
     for i in range(n - 1, 0, -1):
-        j = np.random.randint(0, i)
+        # j = np.random.randint(0, i)
+        j = get_rand_int(i + 1) - 1
+        print(j)
         aux = array[i]
         array[i] = array[j]
         array[j] = aux
