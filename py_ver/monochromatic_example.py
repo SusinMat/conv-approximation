776a9611e99b7e3033d0939c3b991736f1f8da8d
SusinMat <57694766+SusinMat@users.noreply.github.com>
2019-08-08 21:06:28 -0300

    Fix monochromatic_example expecting a smaller list returned from monochromatic_approx

--- py_ver/monochromatic_example.py
+++ py_ver/monochromatic_example.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     W = np.abs(np.random.rand(32, 3, 3, 3))
     print("||W|| == " + str(la.norm(W)))
 
-    [Wapprox, Wmono, colors, perm] = monochromatic_approx(W, num_colors=4, even=False)
+    [Wapprox, Wmono, colors, perm, num_weights] = monochromatic_approx(W, num_colors=4, even=True)
     L2_err = la.norm(W - Wapprox) / la.norm(W)
 
     print("||W - Wapprox|| / ||W|| == " + str(L2_err))
